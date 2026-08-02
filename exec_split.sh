3eab7cc3c20d505c92b7d5dd94e482fd3363f715
Pedro Cardoso <pedro.cardoso.dev@gmail.com>
2026-08-02 17:51:38 -0300

    Convert .part files from Git LFS to standard Git files

--- exec_split.sh
+++ exec_split.sh
@@ -1,2 +1,2 @@
-split -C 95m -d --additional-suffix=.part data/wikipedia_index.jsonl data/wikipedia_index.jsonl 
-split -C 95m -d --additional-suffix=.part data/igdb_index.jsonl data/igdb_index.jsonl
+split -C 50m -d --additional-suffix=.part data/wikipedia_index.jsonl data/wikipedia_index.jsonl 
+split -C 50m -d --additional-suffix=.part data/igdb_index.jsonl data/igdb_index.jsonl
