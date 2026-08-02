b19c0472a8fe5c7a4b29a65e03453cb5803f4b19
Pedro Cardoso <pedro.cardoso.dev@gmail.com>
2026-08-02 13:46:52 -0300

    fix: correct formatting and quotes in setup script for environment variables

--- setup.sh
+++ setup.sh
@@ -2,13 +2,14 @@
 
 FILE_NAME=".env"
 
-FILE_TEMPLATE="# --- Gemini ---
-GEMINI_API_KEY=<your_api_key>
+read -r -d '' FILE_TEMPLATE << EOM
+# --- Gemini ---
+GEMINI_API_KEY='<your_api_key>'
 
 # --- IGDB ---
 # Only fill these ones if you intend to ingest IGDB data yourslef.
-IGDB_CLIENT_ID=<optional_api_key> 
-IGDB_CLIENT_SECRET=<optional_client_secret>
+IGDB_CLIENT_ID='<optional_api_key>'
+IGDB_CLIENT_SECRET='<optional_client_secret>'
 
 # Do not change the fields below
 
@@ -18,20 +19,59 @@ POSTGRES_USER=user
 
 # --- Opensearch ---
 OPENSEARCH_USER=admin
-OPENSEARCH_PASSWORD='Opensearch16admin#'"
+OPENSEARCH_PASSWORD='Opensearch16admin#'
+EOM
 
 if [ -f "${FILE_NAME}" ]; then
     read -p "File $FILE_NAME already exists. Overwrite? (y/N): " response
 
     if [[ "$response" != "y" && "$response" != "Y" ]]; then
         echo "Skipping file creation."
-        exit 0
+    else 
+        # Create .env file
+        echo "$FILE_TEMPLATE" > .env
     fi     
 fi
 
-# Create .env file
-echo "$FILE_TEMPLATE" > .env
-source .env
+STREAMLIT_FILE=".streamlit/secrets.toml"
+
+read -r -d '' STREAMLIT_TEMPLATE << EOM
+[postgres]
+dialect = "postgresql"
+host = "postgres"
+port = 5432
+database = "usage"
+user = "user"
+password = "postgres"
+
+[postgres_evaluations]
+dialect = "postgresql"
+host = "postgres"
+port = 5432
+database = "evaluations"
+user = "user"
+password = "postgres"
+EOM
+
+mkdir -p .streamlit
+
+if [ -f "${STREAMLIT_FILE}" ]; then
+    read -p "File $STREAMLIT_FILE already exists. Overwrite? (y/N): " response
+    
+    if [[ "$response" == "y" || "$response" == "Y" ]]; then
+        echo "$STREAMLIT_TEMPLATE" > "$STREAMLIT_FILE"
+    else
+        echo "Skipping file creation."
+    fi
+else
+    echo "$STREAMLIT_TEMPLATE" > "$STREAMLIT_FILE"
+fi
+
+
+# Load environment variables if .env exists
+if [ -f .env ]; then
+    source .env
+fi
 
 # Necessary for opensearch hybrid search (must have extra memory).
 sudo sysctl -w vm.max_map_count=262144
